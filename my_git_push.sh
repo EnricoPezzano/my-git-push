40a8681161854c828ab1483e2dcd69b44d5ba7cf
Enrico Pezzano <53240167+EnricoPezzano@users.noreply.github.com>
2022-02-03 10:23:23 +0100

    Add files via upload

---
+++ my_git_push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git add *
+
+# echo 'Enter the commit message:'
+# read commitMessage
+# git commit -a "$commitMessage"
+
+git commit -am "enrico's script push"
+
+# echo 'Enter the name of the branch:'
+# read branch
+
+git push #origin $branch
